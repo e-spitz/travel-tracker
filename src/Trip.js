ed70ceff27d4254dc0c4df438573946758e920c7
Erica <ericaspitz522@gmail.com>
2021-08-04 13:30:25 -0600

    Try test again

--- src/Trip.js
+++ src/Trip.js
@@ -7,7 +7,7 @@ class Trip {
     this.date = singleTrip.date;
     this.duration = singleTrip.duration;
     this.status = singleTrip.status;
-    this.suggestedActivites = singleTrip.suggestedActivities;
+    this.suggestedActivites = [];
   }
 }
 
