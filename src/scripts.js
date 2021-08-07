37fb0e2476f87c2d886d2ad14abcd89b3ec36eba
Erica <ericaspitz522@gmail.com>
2021-08-07 16:43:54 -0600

    Add travelers and traveler fetch call to find all travelers and random traveler

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,7 @@ import Traveler from './Traveler';
 import Destination from './Destination';
 
 let traveler;
+let travelers;
 let allDestinations;
 let allTrips;
 let date;
@@ -17,17 +18,26 @@ window.addEventListener('load', () => {
 const start = () => {
   setUpTrips();
   setUpDestinations();
+  setUpTraveler();
 }
 
 const setUpTrips = () => {
   fetchAPIData('trips')
   .then(data => allTrips = data.trips)
+  .then(data => console.log('allTrips', allTrips)) // single traveler trips instantiated in Traveler class
 }
 
 const setUpDestinations = () => {
   fetchAPIData('destinations')
   .then(data => allDestinations = data.destinations)
-  .then(data => console.log(allDestinations))
+  .then(data => console.log('allDestinations', allDestinations)) //don't think we need to instantiate here bc all properties are accessible through dot notation
+}
+
+const setUpTraveler = () => {
+  fetchAPIData('travelers')
+  .then(data => travelers = data.travelers.map(trav => new Traveler(trav)))
+  .then(data => traveler = travelers[Math.floor(Math.random() * travelers.length)])
+  .then(data => console.log('single traveler', traveler, 'allTravelers', travelers))
 }
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
