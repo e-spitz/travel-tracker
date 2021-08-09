272c501bf3679f7174b9465d0d0c32251c37d048
Erica <ericaspitz522@gmail.com>
2021-08-08 18:18:52 -0600

    Finish promise.all and add in error handling function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,24 @@
-export const fetchAPIData = (type) => {
+const fetchAPIData = (type) => {
   return fetch(`http://localhost:3001/api/v1/${type}`)
     .then(response => response.json())
-    .catch(error => console.log('error: ', error))
+    .catch(err => displayError(err))
 }
+
+const fetchAll = () => {
+  return Promise.all([
+    fetchAPIData('travelers/16'),
+    fetchAPIData('travelers'),
+    fetchAPIData('trips'),
+    fetchAPIData('destinations'),
+  ])
+  .catch(err => displayError(err))
+}
+
+const displayError = (errMsg) => {
+    const bookingError =  document.getElementById('bookingError');
+    const msg = errMsg.message === 'Failed to fetch' ?
+      "Internet connection may be unstable. Please try again later." : errMsg;
+    bookingError.innerText = `Something went wrong, please try again later.`;
+}
+
+export { fetchAll }
